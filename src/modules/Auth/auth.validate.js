53d318a94b1bb142e6fa7453a9b0d101d708e025
bluecrypt47 <lechuong09291999@gmail.com>
2021-01-19 20:20:18 +0700

    Xong validate

--- src/modules/Auth/auth.validate.js
+++ src/modules/Auth/auth.validate.js
@@ -1,28 +1,55 @@
 import Result from '../../helpers/result.helper';
 
+function checkEmail() {
+  var email = document.getElementById('email');
+  var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  if (!filter.test(email.value)) {
+    alert('Hãy nhập đúng đinh dạng: abc@gmail.com');
+    email.focus;
+    return false;
+  }
+}
+
 function validateFormRegister(req, res, next) {
   try {
-    const { fullname, email, password } = req.body;
-    if ((fullname === '' || email === '', password === '')) {
+    const { fullname, email, password, retypePassword } = req.body;
+    if ((fullname === '' || email === '', password === '', retypePassword === '')) {
       return Result.error(res, { message: 'Không được để trống' });
     }
+    if (2 <= fullname.length <= 30) {
+      return Result.error(res, { message: 'Họ tên phải có độ dài 2-30 ký tự!' });
+    }
+    if (6 <= email.length <= 35) {
+      return Result.error(res, { message: 'Email phải có độ dài từ 6-35 ký tự!' });
+    }
+    if (6 <= password.length <= 30) {
+      return Result.error(res, { message: 'Mật khẩu phải là chuỗi có độ dài từ 6-30 ký tự!' });
+    }
+    checkEmail();
     next();
   } catch (error) {
     next(error);
   }
 }
 
-function validateFromLogin(req, res, next){
-  try{
-    const{ email, password} = req.body;
-    if((email === '' || password === '' || email === '', password ==='')){
-      return Result.error(res, {message: 'Vui lòng nhập đầy đủ tài khoản, mật khẩu và được để trống!'});
+function validateFromLogin(req, res, next) {
+  try {
+    const { email, password } = req.body;
+    if ((email === '' || password === '' || email === '', password === '')) {
+      return Result.error(res, { message: 'Vui lòng nhập đầy đủ tài khoản, mật khẩu và được để trống!' });
     }
+    if (6 <= email.length <= 35) {
+      return Result.error(res, { message: 'Email phải từ 6-35 ký tự!' });
+    }
+    if (6 <= password.length <= 30) {
+      return Result.error(res, { message: 'Mật khẩu phải là chuỗi có độ dài từ 6-30 ký tự!' });
+    }
+    checkEmail();
     next();
-  }catch(error){
+  } catch (error) {
     next(error);
   }
 }
 
-const Authvalidate={validateFormRegister, validateFromLogin};
+const Authvalidate = { validateFormRegister, validateFromLogin, checkEmail };
 export default Authvalidate;
--- src/modules/User/user.validate.js
+++ src/modules/User/user.validate.js
@@ -1,4 +1,5 @@
 import Result from '../../helpers/result.helper';
+import Authvalidate from '../auth/auth.validate';
 
 function validateUpdateInfor(req, res, next) {
   try {
@@ -6,6 +7,16 @@ function validateUpdateInfor(req, res, next) {
     if ((fullname == '', email === '', bio === '')) {
       return Result.error(res, { message: 'Vui lòng nhập ít nhất một thông tin để cập nhật!' });
     }
+    if (2 <= fullname.length <= 30) {
+      return Result.error(res, { message: 'Họ tên phải có độ dài 2-30 ký tự!' });
+    }
+    if (6 <= email.length <= 35) {
+      return Result.error(res, { message: 'Email phải có độ dài từ 6-35 ký tự!' });
+    }
+    if (2 <= bio.length <= 50) {
+      return Result.error(res, { message: 'Bio phải có độ dài từ 2-50 ký tự!' });
+    }
+    Authvalidate.checkEmail();
     next();
   } catch (error) {
     next(error);
@@ -14,13 +25,27 @@ function validateUpdateInfor(req, res, next) {
 
 function validateUploadPassword(req, res, next) {
   try {
-    const { currentPassword, newPassword } = req.body;
+    const { currentPassword, newPassword, retypePassword } = req.body;
     if (currentPassword === '') {
       return Result.error(res, { message: 'Vui lòng không để trống mậu khẩu hiện tại trước khi đổi!' });
-    } else if (newPassword === '') {
+    }
+    if (newPassword === '') {
       return Result.error(res, { message: 'Vui lòng không để trống mậu khẩu mới trước khi đổi!' });
-    } else if ((currentPassword === '', newPassword === '')) {
+    }
+    if (retypePassword === '') {
+      return Result.error(res, { message: 'Vui lòng không để trống nhập lại mậu khẩu mới trước khi đổi!' });
+    }
+    if ((currentPassword === '', newPassword === '', retypePassword)) {
       return Result.error(res, { message: 'Vui lòng nhập mật khẩu trước khi đổi!' });
+    }
+    if (2 <= currentPassword.length <= 30) {
+      return Result.error(res, { message: 'Mật khẩu cũ phải có độ dài 2-30 ký tự!' });
+    }
+    if (2 <= newPassword.length <= 30) {
+      return Result.error(res, { message: 'Mật khẩu mới phải có độ dài 2-50 ký tự!' });
+    }
+    if (newPassword === retypePassword) {
+      return Result.error(res, { message: 'Vui lòng nhập đúng mật khẩu mới!' });
     }
     next();
   } catch (error) {

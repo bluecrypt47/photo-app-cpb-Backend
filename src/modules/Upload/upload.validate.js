40c58170184419706e964af2e3bf19fff6a588f4
bluecrypt47 <lechuong09291999@gmail.com>
2021-01-19 11:42:17 +0700

    làm được user nhưng xem lại updateAvatar

---
+++ src/modules/Upload/upload.validate.js
@@ -0,0 +1,15 @@
+
+import Result from '../../helpers/result.helper';
+
+function validateUpload(req, res, next){
+    try{
+        const{path} = req.body;
+        if(path === ''){
+            return Result.error(res, {message: 'Vui lòng nhập đường dẫn của ảnh trước khi cập nhập ảnh đại diện!'});
+        }
+        next();
+    }catch(error){
+        next(error);
+    }
+    
+}
